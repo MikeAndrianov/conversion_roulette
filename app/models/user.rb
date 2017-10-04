8a73434973db0976c2af76266b1aeb92929c6649
mike <mikeandrianov@gmail.com>
2017-10-04 22:58:25 +0300

    Add Rubocop, fix code issues

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,18 +3,19 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :authentication_keys => [:login]
+         authentication_keys: [:login]
 
   validates :username, presence: true, uniqueness: { case_sensitive: false }
   validate :validate_username
 
   def self.find_for_database_authentication(warden_conditions)
     conditions = warden_conditions.dup
-    if login = conditions.delete(:login)
-      where(conditions.to_hash).where(['lower(username) = :value OR lower(email) = :value', { value: login.downcase }]).first
-    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
-      conditions[:email].downcase! if conditions[:email]
-      where(conditions.to_hash).first
+    login = conditions.delete(:login)
+
+    if login
+      find_user_by_login
+    elsif conditions.key?(:username) || conditions.key?(:email)
+      find_user_by_username_or_email
     end
   end
 
@@ -22,5 +23,16 @@ class User < ApplicationRecord
 
   def validate_username
     errors.add(:username, :invalid) if User.where(email: username).exists?
+  end
+
+  def find_user_by_login
+    where(conditions.to_hash)
+      .where(['lower(username) = :value OR lower(email) = :value', { value: login.downcase }])
+      .first
+  end
+
+  def find_user_by_username_or_email
+    conditions[:email].downcase! if conditions[:email]
+    where(conditions.to_hash).first
   end
 end
